0a789f5301d5116121e89ea110d865267d10ef0f
Angel David Rojas Riveros <halkdavid190@gmail.com>
2022-11-28 19:45:16 -0500

    v1.0.8

--- Assets/Scripts/platfor/FailPlataforma.cs
+++ Assets/Scripts/platfor/FailPlataforma.cs
@@ -6,22 +6,20 @@ public class FailPlataforma : MonoBehaviour
 {
     Rigidbody2D rb;
     float fallDelay = 2f;
-    private Animator Anim;
+    [SerializeField] private Animator Anim;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
+        Anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    IEnumerator Fall()
+    IEnumerator ConstPlat()
     {
 
-        yield return new WaitForSeconds(0.2f);
-
-        rb.bodyType = RigidbodyType2D.Dynamic;
-        Destroy(gameObject, fallDelay);
+        yield return new WaitForSeconds(1.5f);
+        Anim.SetBool("Destruir",false);
 
     }
     private void OnCollisionEnter2D(Collision2D collision)
@@ -29,6 +27,7 @@ public class FailPlataforma : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             Anim.SetBool("Destruir",true);
+            StartCoroutine(ConstPlat());
         }
     }
     public void ConsPlat()
