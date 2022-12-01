459b21797d74b6309afbdc34df5f03c0a332a36d
Angel David Rojas Riveros <halkdavid190@gmail.com>
2022-12-01 13:47:57 -0500

    v1.1.1

--- Assets/Scripts/SceneObjects/Cofre.cs
+++ Assets/Scripts/SceneObjects/Cofre.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Cofre : MonoBehaviour
 {
@@ -7,17 +6,17 @@ public class Cofre : MonoBehaviour
     public GameObject Coin;
 
     [SerializeField] private bool ConKey;
-    [SerializeField] private Image Key;
+    //[SerializeField] private Image Key;
     // Start is called before the first frame update
     void Start()
     {
         Anim = GetComponent<Animator>();
     }
-    public void KeyDrop()
+    /*public void KeyDrop()
     {
         if(ConKey== true)
         {
-            Key.color = new Color(255, 255, 255);
+            //Key.color = new Color(255, 255, 255);
         }
-    }
+    }*/
 }
